2f5d7fafb7ecc88d02989609ce4b14c03df6dcd2
rohan22 <rohan@novumlogic.com>
2023-05-15 16:27:30 +0530

    Dynamic Why choose us counter
    For white template.

--- component-template-2/Whychooseus.js
+++ component-template-2/Whychooseus.js
@@ -18,13 +18,16 @@ function Whychooseus({ whyChooseUsData }) {
             {whyChooseUsData.component.map((whychooseus, index) =>{
                count++
               return <React.Fragment key={index}>
-                <div className="card py-3">
-                  <div>
-                    <Image className="why-us-sticker" src={sticker} alt="Card image cap" width={150} height={150} />
+                <div className="card py-3 d-flex flex-row justify-content-centent align-items-center">
+                  <div className="why-us-main">
+                    <div className="why-us-desc">
+                    {/* <Image className="why-us-sticker" src={sticker} alt="Card image cap" width={150} height={150} /> */}
+                    <h2 className=" fw-bold">{whychooseus.description}</h2>
+                    </div>
                   </div>
-                  <div className="img-square-wrapper d-flex align-items-center">
-                    <h2 className="display-5 fw-bold">{count}</h2>
-                    <h3>{whychooseus.title}</h3>
+                  <div className="img-square d-flex align-items-center">
+                  
+                    <h3 className="img-square-text">{whychooseus.title}</h3>
                   </div>
                 </div>
                 
@@ -41,6 +44,55 @@ function Whychooseus({ whyChooseUsData }) {
   <style jsx global>
   {
     `
+    .img-square-text
+    {
+      font-size: 1.75rem;
+    }
+    .why-us-main
+    {
+      z-index: 1;
+    }
+    .why-us-desc
+    {
+      background: url('img-tamplate-2/assets-temp-2/sticker.png');
+                            max-width: 150px;
+                            width: 150px;
+                            height: 150px;
+                            background-size: contain;
+                            background-repeat: no-repeat;
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            z-index: 1;
+    }
+    .img-square
+    {
+      background-color: rgba(255,204,0, 0.4) !important;
+                        margin-left: -50px;
+                        margin-top: 16px;
+                        padding: 10px;
+                        padding-left: 61px;
+                        margin-bottom: 16px;
+                        border-radius: 15px;
+                        min-height: 120px;
+    }
+    @media (max-width:900px) {
+      .why-us-desc
+      {
+        max-width: 120px;
+                            width: 120px;
+                            height: 120px;
+      }
+      .img-square-text
+      {
+        font-size: 1.0rem;
+      }
+      .img-square
+      {
+        min-height: 105px;
+      }
+    }
+
     @media (max-width:500px) {
       .display-big
       {
